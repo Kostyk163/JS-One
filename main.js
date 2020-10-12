9a647c53ae48dfb4395390510f4ba0f6e317629b
Kostyk163 <46649456+Kostyk163@users.noreply.github.com>
2020-10-12 13:03:52 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+//var name;
+//var admin;
+//name = "Vasiliy";
+//admin = name;
+//alert(admin);
+
+let celsius = prompt('Enter value: ');
+let faringate = (9/5)*celsius+32;
+
+alert('Temp of a faringate: ' + faringate);
+
+let name = ('Vasiliy');
+let admin = name;
+alert(admin);
+
+const sum = 1000 + '108';
+alert(sum);
