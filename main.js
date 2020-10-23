e60bf6c2ea9d308fc40cbd38210361da5cf686e9
Konstantin Malyshev <46649456+Kostyk163@users.noreply.github.com>
2020-10-23 16:12:21 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-//var name;
-//var admin;
-//name = "Vasiliy";
-//admin = name;
-//alert(admin);
+var a = 1, b = 1, c, d;
+c = ++a; alert(c);            // c = 1+1 = 2
+d = b++; alert(d);            // d = 0+1 = 1
+c = (2 + ++a); alert(c);      // c = 2+(1+2) = 5
+d = (2 + b++); alert(d);      // d = 2+(1+1) = 4
+alert(a);                     // 3 - последний раз на 4 строке а было равно 3
+alert(b);                     // 3 - ?
 
-let celsius = prompt('Enter value: ');
-let faringate = (9/5)*celsius+32;
 
-alert('Temp of a faringate: ' + faringate);
+var a = 2;
+var x = 1 + (a *= 2); // 5
+alert('x = ' + x);
 
-let name = ('Vasiliy');
-let admin = name;
-alert(admin);
+let a = prompt('Введите a');
+let b = prompt('Введите b');
 
-const sum = 1000 + '108';
+if (a > 0 && b > 0) {
+    sum = a - b;
+} else if (a < 0 && b < 0) {
+    sum = a * b;
+} else if ((a < 0 && b > 0) && (a > 0 && b <0)) {
+    sum = a + b;
+}
+
 alert(sum);
+
