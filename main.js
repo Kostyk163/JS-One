77ce614f4686d2b7a86333786acb8f7938047b99
Konstantin Malyshev <46649456+Kostyk163@users.noreply.github.com>
2020-10-28 18:15:22 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-//var name;
-//var admin;
-//name = "Vasiliy";
-//admin = name;
-//alert(admin);
-
-let celsius = prompt('Enter value: ');
-let faringate = (9/5)*celsius+32;
-
-alert('Temp of a faringate: ' + faringate);
-
-let name = ('Vasiliy');
-let admin = name;
-alert(admin);
-
-const sum = 1000 + '108';
-alert(sum);
+let max = 999;
+let numb = {
+    number: prompt('Введите число от 0 до 999'),
+    units: 0,
+    tens: 0,
+    hundreds: 0,
+};
+if (numb.number <= 9){
+    numb.units = numb.number;
+} else if (numb.number <= 999){
+    numb.units = Math.floor(numb.number % 10);
+    numb.tens = Math.floor(numb.number / 10 % 10);
+    numb.hundreds = Math.floor(numb.number / 100 % 10);
+} else {
+    numb.number = 0;
+    alert('Вы ввели число за диапазоном 0 - 999');
+}
+alert(numb);
